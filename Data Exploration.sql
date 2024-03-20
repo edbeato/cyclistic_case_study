f36eee57a3e39e395e8b42e3473a06dd4bcb3ddd
Eduardo Beato <163080154+edbeato@users.noreply.github.com>
2024-03-20 14:39:38 -0400

    Add files via upload

---
+++ Data Exploration.sql
@@ -0,0 +1,16 @@
+-- Counting all null values
+
+SELECT COUNT(*) - COUNT(ride_id) AS ride_id,
+ COUNT(*) - COUNT(rideable_type) AS rideable_type,
+ COUNT(*) - COUNT(started_at) AS started_at,
+ COUNT(*) - COUNT(ended_at) AS ended_at,
+ COUNT(*) - COUNT(start_station_name) AS start_station_name,
+ COUNT(*) - COUNT(start_station_id) AS start_station_id,
+ COUNT(*) - COUNT(end_station_name) AS end_station_name,
+ COUNT(*) - COUNT(end_station_id) AS end_station_id,
+ COUNT(*) - COUNT(start_lat) AS start_lat,
+ COUNT(*) - COUNT(start_lng) AS start_lng,
+ COUNT(*) - COUNT(end_lat) AS end_lat,
+ COUNT(*) - COUNT(end_lng) AS end_lng,
+ COUNT(*) - COUNT(member_casual) AS member_casual
+FROM `oceanic-beach-410402.Cyclistic.combined_2023`
