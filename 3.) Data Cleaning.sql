06d6eebbaaf22b760b457924893fc13e5eeb16cd
Eduardo Beato <163080154+edbeato@users.noreply.github.com>
2024-03-22 14:28:53 -0400

    Update 3.) Data Cleaning.sql

--- 3.) Data Cleaning.sql
+++ 3.) Data Cleaning.sql
@@ -17,32 +17,7 @@ SELECT
   end_lat,
   end_lng,
   ended_at - started_at AS ride_duration,
-  CASE EXTRACT(HOUR FROM started_at) 
-      WHEN 0 THEN '12 AM'
-      WHEN 1 THEN '1 AM'
-      WHEN 2 THEN '2 AM'
-      WHEN 3 THEN '3 AM'
-      WHEN 4 THEN '4 AM'
-      WHEN 5 THEN '5 AM'
-      WHEN 6 THEN '6 AM'
-      WHEN 7 THEN '7 AM'
-      WHEN 8 THEN '8 AM'
-      WHEN 9 THEN '9 AM'
-      WHEN 10 THEN '10 AM'
-      WHEN 11 THEN '11 AM'
-      WHEN 12 THEN '12 PM'
-      WHEN 13 THEN '1 PM'
-      WHEN 14 THEN '2 PM'
-      WHEN 15 THEN '3 PM'
-      WHEN 16 THEN '4 PM'
-      WHEN 17 THEN '5 PM'
-      WHEN 18 THEN '6 PM'
-      WHEN 19 THEN '7 PM'
-      WHEN 20 THEN '8 PM'
-      WHEN 21 THEN '9 PM'
-      WHEN 22 THEN '10 PM'
-      WHEN 23 THEN '11 PM'
-    END AS start_hour,
+  EXTRACT(HOUR FROM started_at) AS start_hour,
   CASE EXTRACT(DAYOFWEEK FROM started_at) 
       WHEN 1 THEN 'MON'
       WHEN 2 THEN 'TUE'
